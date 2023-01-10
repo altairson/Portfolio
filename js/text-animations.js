11757b43f68ef88541488ba8c1dab0caf3a927aa
Su1eiman <smm8650@gmail.com>
2023-01-11 02:44:37 +0400

    Color

--- js/text-animations.js
+++ js/text-animations.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var text = $('#text')[0];
     var shadow = '';
     for(var i = 0; i < 25; i++) { 
-        shadow += (shadow ? ',' : '') + -i * 1 + 'px ' + i * 1 + 'px 0 #d9d9d9';
+        shadow += (shadow ? ',' : '') + -i * 1 + 'px ' + i * 1 + 'px 0 #444';
     }
     text.style.textShadow = shadow;
 })
